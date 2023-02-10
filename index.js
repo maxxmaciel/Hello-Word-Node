48bc8d6234122a1be0033b53f31dfb98652468ba
maxxmaciel <88160151+maxxmaciel@users.noreply.github.com>
2023-02-10 10:22:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+// const path = require("path");
+const app = express();
+app.use(express.static('html'))
+
+app.use (
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
+
+app.get('/index.html', (req, res) => {
+    console.log(req)
+    
+    // mostrar req 
+    res.sendFile(__dirname + "/public/index.html")
+    // res.json({message: 'Oi express!'})
+})
+
+app.get('/', (req, res) => {
+    console.log(req)
+    
+    // mostrar req 
+    res.json({message: 'Oi express!'})
+})
+
+
+app.listen(3000);
